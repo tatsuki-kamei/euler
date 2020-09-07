dd3b4dd6432d97f893e52f79757b07c51c18b5f5
tatsuki-kamei <kameitatsuki0624@gmail.com>
2020-09-07 11:16:11 +0900

    problem12-13 end

--- problem12.rb
+++ problem12.rb
@@ -2,13 +2,19 @@ require 'prime'
 def pri_div(n)
 	sum = 1
 	m = n.prime_division
-	m.each do a
-		sum *= (a[1].to_i + 1)
+	m.length.times do |a|
+		sum *= (m[a][1].to_i + 1)
 	end
 	return sum
 end
 n = 1
-while false
-	n += 1
+x = 1
+while true
+	x+= 1
+	n += x
 	if pri_div(n) >= 500
-		
+		puts n
+		return false
+	end
+end
+
